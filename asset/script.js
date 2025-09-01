a4dbdc5280128fbf8185dffb44954e02e16e0a40
kakoikeisuke <k.kakoi0531@gmail.com>
2025-09-01 20:02:16 +0900

    ファイルを読み取った際のエラーメッセージ処理を追加

--- asset/script.js
+++ asset/script.js
@@ -4,3 +4,47 @@ const inputFile = document.getElementById('input-file');
 inputView.addEventListener('click', () => {
     inputFile.click();
 });
+
+// 画像が入力されると, 画像がpng画像か確認
+inputFile.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+        errorMessage("正常に画像が読み込まれませんでした。");
+        return;
+    }
+    if (file.type !== 'image/png') {
+        errorMessage("PNG画像を選択してください。");
+        inputFile.value = '';
+        return;
+    }
+    errorMessage("正常に読み込まれました。");
+});
+
+// エラーがあった際の表示
+function errorMessage(message) {
+    const backgroundFilter = document.createElement("div");
+    backgroundFilter.className = "background-filter";
+    document.body.appendChild(backgroundFilter);
+    const errorMessageDiv = document.createElement("div");
+    errorMessageDiv.className = "error-message-div";
+    backgroundFilter.appendChild(errorMessageDiv);
+    const errorMessageText = document.createElement("p");
+    errorMessageText.textContent = message;
+    errorMessageDiv.appendChild(errorMessageText);
+    const closeButton = document.createElement("p");
+    closeButton.className = "close-button";
+    closeButton.textContent = '閉じる';
+    closeButton.addEventListener('click', closeErrorMessage);
+    errorMessageDiv.appendChild(closeButton);
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            closeErrorMessage();
+        }
+    });
+}
+function closeErrorMessage() {
+    const backgroundFilter = document.querySelector(".background-filter");
+    if (backgroundFilter) {
+        backgroundFilter.remove();
+    }
+}
